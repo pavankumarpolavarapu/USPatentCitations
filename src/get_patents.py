1b785deffc4da6b9fb2d6fbe32e5759a9472424e
Pavan Kumar Polavarapu <pavan.vnr@gmail.com>
2019-06-15 21:39:57 -0400

    004 - Added logic to work with recent two versions of USPTO files

--- src/get_patents.py
+++ src/get_patents.py
@@ -5,8 +5,8 @@ from patent_parser import read_patent
 
 def main():
     df_filelist = pd.read_csv("patent_weekly_list.csv", header=0)
-    download_patent_file(df_filelist.tail(1))
-    read_patent(df_filelist.tail(1))
+    download_patent_file(df_filelist.iloc[[40]])
+    read_patent(df_filelist.iloc[[40]])
 
 
 if __name__ == '__main__':
--- src/patent_parser.py
+++ src/patent_parser.py
@@ -11,15 +11,21 @@ def read_patent(file):
     storage_path = os.path.join(os.getcwd(), storage_dir)	
     path = os.path.join(storage_path, file.name.values[0])
     #declaring XMLPullParser with end event
-    pat_xml = etree.XMLPullParser(tag='us-patent-grant', events=['end'], recover=True)
+    if file.name.values[0].startswith("pgb"):
+        pat_xml = etree.XMLPullParser(tag='PATDOC', events=['end'], recover=True)
+    elif file.name.values[0].startswith("ipgb"):
+        pat_xml = etree.XMLPullParser(tag='us-patent-grant', events=['end'], recover=True)
     with open(path, 'r') as lines:
         for line in lines:
             if line.startswith("<?xml"):
                 if(patent_ends(pat_xml, file.name.values[0])):
                     pat_xml.close()
-                    pat_xml = etree.XMLPullParser(tag='us-patent-grant', events=['end'], recover=True)
+                    if file.name.values[0].startswith("pgb"):
+                        pat_xml = etree.XMLPullParser(tag='PATDOC', events=['end'], recover=True)
+                    elif file.name.values[0].startswith("ipgb"):
+                        pat_xml = etree.XMLPullParser(tag='us-patent-grant', events=['end'], recover=True)
             #Removing unwanted nodes
-            elif line.startswith("<!DOCTYPE") or line.startswith("]>"):
+            elif line.startswith("<!DOCTYPE") or line.startswith("]>") or line.startswith('<!ENTITY'):
                 pass
             else:
                 #pudb.set_trace()
@@ -35,38 +41,107 @@ def patent_ends(pat_xml, file):
 
 def init_patent_processing(element, file):
     if(file.startswith("ipgb")):
-        process_patent_version1(element)
-    elif fname.startswith('pgb'):
+        #process_patent_version1(element)
+        pass
+    elif (file.startswith('pgb')):
         process_patent_version2(element)
 
 def process_patent_version1(element):
+    patents = defaultdict(str)
     patent_number = element.find('us-bibliographic-data-grant') \
                     .find('publication-reference')\
                     .find('document-id')\
                     .find('doc-number').text
 
+    # Important Info
+    patents['title'] = result_or_default(element.find('us-bibliographic-data-grant'), 'invention-title')
+
+    inventors = element.find('us-bibliographic-data-grant').find('us-parties').find('inventors')
+    if inventors is not None:
+        patents['inventor'] = []
+        for inventor in inventors.findall('inventor'):
+            addressbook = inventor.find('addressbook')
+            if addressbook is not None:
+                patents['inventor'].append([result_or_default(addressbook, 'last-name'), result_or_default(addressbook, 'first-name')])
+    
+    applicants = element.find('us-bibliographic-data-grant').find('us-parties').find('us-applicants')
+    if applicants is not None:
+        patents['applicant'] = []
+        for applicant in applicants.findall('us-applicant'):
+            addressbook = applicant.find('addressbook')
+            if addressbook is not None:
+                patents['applicant'].append([result_or_default(addressbook, 'last-name'), result_or_default(addressbook, 'first-name')])
+
+    assignee = element.find('us-bibliographic-data-grant').find('assignees/assignee/addressbook')
+    if assignee is not None:
+        patents['owner'] = result_or_default(assignee, 'orgname')
+
+    
+    
     # citations
-    refs = element.find('us-bibliographic-data-grant').find('references-cited')
+    citation_references = element.find('us-bibliographic-data-grant').find('references-cited')
     prefix = ''
-    if refs is None:
-        refs = element.find('us-bibliographic-data-grant').find('us-references-cited')
+    if citation_references is None:
+        citation_references = element.find('us-bibliographic-data-grant').find('us-references-cited')
         prefix = 'us-'
 
     citations = []
-    if refs is not None:
-        for citation in refs.findall(prefix + 'citation'):
+    if citation_references is not None:
+        for citation in citation_references.findall(prefix + 'citation'):
             pcitation = citation.find('patcit')
             if pcitation is not None:
                 docid = pcitation.find('document-id')
                 patent_num = result_or_default(docid, 'doc-number')
                 citations.append(patent_num)
-    patents = defaultdict(str)
+        patents['citations'] = citations    
+    
     patents['number'] = patent_number
-    patents['citations'] = citations
-    write_citations(patents)
 
-def process_patent_version2():
-    pass
+    
+    write_citations(patents)
+    write_patent_header(patents)
+
+
+def process_patent_version2(element):
+    patents = defaultdict(str)
+    patent_number = result_or_default(element.find('SDOBI').find('B100'), 'B110/DNUM/PDAT')
+    
+    #important info
+    patents['title'] = result_or_default(element.find('SDOBI').find('B500'), 'B540/STEXT/PDAT')
+
+    inventors = element.find('SDOBI').find('B700/B720/B721')
+    if inventors is not None:
+        patents['inventor'] = []
+        for inventor in inventors.findall('PARTY-US'):
+            details = inventor.find('NAM')
+            if details is not None:
+                patents['inventor'].append([result_or_default(details, 'FNAM/PDAT'), result_or_default(details, 'SNM/STEXT/PDAT')])
+    
+    applicants = element.find('SDOBI').find('B700/B730/B731')
+    if applicants is not None:
+        patents['applicant'] = []
+        for applicant in applicants.findall('PARTY-US'):
+            details = applicant.find('NAM')
+            if details is not None:
+                patents['applicant'].append([result_or_default(details, 'FNAM/PDAT'), result_or_default(details, 'SNM/STEXT/PDAT')])
+    
+    owner = element.find('SDOBI').find('B700/B730/B731/PARTY-US')
+    if owner is not None:
+        patents['owner'] = result_or_default(owner, 'NAM/ONM/STEXT/PDAT')
+    
+    citation_references = element.find('SDOBI').find('B500/B560')
+    if citation_references is not None:
+        citations = []
+        for citation in citation_references.findall('B561'):
+            patent_num = result_or_default(citation, 'PCIT/DOC/DNUM/PDAT')
+            citations.append(patent_num)
+        patents['citations'] = citations
+    
+    patents['number'] = patent_number
+    
+    
+    write_citations(patents)
+    write_patent_header(patents)    
 
 def process_patent_version3():
     pass
@@ -107,4 +182,13 @@ def write_citations(patents):
         writer = csv.writer(writeFile)
         for citation in patents['citations']:
             writer.writerow([patent_number, citation])
+    writeFile.close()
+
+def write_patent_header(patents):
+    line = []
+    patent_number = patents['number']
+    print(patent_number)
+    with open('patents.csv', 'a') as writeFile:
+        writer = csv.writer(writeFile)
+        writer.writerow([patent_number, patents['title'], patents['inventor'], patents['applicant'], patents['owner']])
     writeFile.close()
